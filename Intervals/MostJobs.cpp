852b6b7e2c5be349c2bfaad19973c9dbd8c883d9
Stephen Blythe <sblythe@lindenwood.edu>
2024-02-14 11:55:32 -0600

    Started code to find largest number of comaptible intervals

---
+++ Intervals/MostJobs.cpp
@@ -0,0 +1,45 @@
+#include "Interval.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+bool compareByFinish(const Interval &i1, const Interval &i2)
+{
+  return i1.finish() < i2.finish();
+}
+
+int main(int argc, char *argv[])
+{
+  string fName = argv[1];
+
+  ifstream ifile(fName);
+
+  list<Interval> possibleJobs;
+
+  Interval job;
+
+  ifile>>job;
+  while(ifile)
+    {
+      possibleJobs.push_back(job);
+
+      ifile>>job;
+    }
+
+  list<Interval> answer;
+  possibleJobs.sort(compareByFinish);
+
+  for (Interval testJob : possibleJobs)
+    {
+      if ( /* compatible*/)
+	answer.push_back(testJob);
+    }
+  
+  for( Interval i: answer)
+    i.display();
+  
+  return 0;
+}
